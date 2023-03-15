70a1f2dd4ba5352de0a08749559e7ef6b2a2534e
taka01150810 <taka01150810@gmail.com>
2023-03-15 18:48:34 +0900

    入退出画面の雛形作成

---
+++ jupitar_app/app/Http/Controllers/EnterAndExitController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EnterAndExitController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('EnterAndExit.index');
+    }
+}
